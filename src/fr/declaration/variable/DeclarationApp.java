d8a98c5fd5e92a97cc966f0fcccc88494d5ca7fe
chkegb <kegbsuc@gmail.com>
2022-06-10 02:44:37 +0200

    TP3

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,38 @@
+package fr.declaration.variable;
+
+import java.util.Scanner;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		// pour ecrire dans java
+		Scanner console = new Scanner(System.in);
+		System.out.println("Veuillez rentrer un entier i");
+		int i=console.nextInt();
+		// pour passer à la ligne apres avoir entrer la valeur
+		console.nextLine();
+		System.out.println(i);
+		*/
+		// la declaration de TP3 
+	    byte b=10;
+	    short t=-17282;
+	    int i=10;
+	    long l=100L;
+	    double y=1.0D;
+	    boolean top=true;
+	    char c = 'c';
+	    float f=3.5F;
+	    String randomString = "Voici le résultat d’un calcul : 1+5=6";
+	    String chaine ="top";
+	    // afficher les informations 
+	    
+	    System.out.println(b);
+	    System.out.println(t);
+	    System.out.println(t);
+	    System.out.println(randomString);
+
+	}
+
+}
